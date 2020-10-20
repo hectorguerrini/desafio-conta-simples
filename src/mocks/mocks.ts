451723ddf1cf560ec36f44ff4a6ad57262a4d99a
Hector Guerrini Herrera desktop <hectorumaga@hotmail.com>
2020-10-20 13:04:41 -0300

    inicio mock empresa

--- src/mocks/mocks.ts
+++ src/mocks/mocks.ts
@@ -1,4 +1,10 @@
+import { random } from "faker";
 const mocks = {
+    ID: () => random.uuid(),
+    DadoBancario: () => ({
+        Int: () => random.number({min: 100000, max:999999}),
+        Banco: () => 999,       
+    }),
     String: () => 'hector'
   }
 export default mocks;
